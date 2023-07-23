467deaa9a9048b98ab8da7f1710a944ed58009c3
kolyb <kolybenko@mail.ru>
2023-07-23 15:25:58 +0300

    Update 02.js

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,6 +1,5 @@
-let getRandomNumber = Math.floor(Math.random() * 100);
-getRandomNumber % 2 == 0 ? console.log(getRandomNumber,
-    'Это четное число') : console.log(getRandomNumber,
-        'Это нечетное число');
+let number1 = Math.floor(Math.random() * 100);
+number1 % 2 == 0 ? console.log(`${number1}:Это 
+четное число`) : console.log(`${number1}:Это нечетное число`);
 
 
