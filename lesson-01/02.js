020e4beff9bce45a36b2dd68d7e325698d1d8b9a
kolyb <kolybenko@mail.ru>
2023-07-23 15:12:16 +0300

    Create variable getRandomNumber

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,10 +1,6 @@
-//Создай рандомное число при помощи  
-//let number1 = Math.floor(Math.random() * 100)
-//а затем проверь их выводя в консоль: «[вставь сюда полученное число] :
-//Это четное число» или «[вставь сюда полученное число] :Это нечетное число». С каждой перезагрузкой страницы у
-// тебя будет новое число.​
-var getRandomNumber = Math.floor(Math.random() * 100);
-if (getRandomNumber % 2 == 0) {
-    console.log(getRandomNumber, "Это четное число");
-}
-else console.log(getRandomNumber, "Это нечетное число");
+let getRandomNumber = Math.floor(Math.random() * 100);
+getRandomNumber % 2 == 0 ? console.log(getRandomNumber,
+    'Это четное число') : console.log(getRandomNumber,
+        'Это нечетное число');
+
+
