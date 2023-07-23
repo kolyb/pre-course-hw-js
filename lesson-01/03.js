767219b866f19ae325a0413ae1df62b83a165084
kolyb <kolybenko@mail.ru>
2023-07-23 16:03:26 +0300

    Use increment and dicrement

---
+++ lesson-01/03.js
@@ -0,0 +1,9 @@
+//​let number = 1;
+//--number   number--  ++number   number++
+//Получи в консоли 1,3,3,1
+//❗Нужно вызвать консоль ЧЕТЫРЕ раза
+let number = 1;
+console.log(number++);
+console.log(++number);
+console.log(number--);
+console.log(--number);
