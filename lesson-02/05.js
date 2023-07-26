64b3e7670f4b99a59ba6dfe4de9b3bf42dbe6a5a
kolyb <kolybenko@mail.ru>
2023-07-26 22:42:13 +0300

    Create objects

---
+++ lesson-02/05.js
@@ -0,0 +1,11 @@
+ let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+let newPassport = {};
+newPassport.name = "Ivan";
+newPassport.surname = passport.surname;
+
+console.log(passport);
+console.log(newPassport);
