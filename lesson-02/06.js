4614f01844d49e040626280cc5dc124206f4d7a7
kolyb <kolybenko@mail.ru>
2023-07-27 21:44:43 +0300

    Create function deepClone

---
+++ lesson-02/06.js
@@ -0,0 +1,28 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+let getNewPassport = deepClone(passportWithAddress);
+
+function deepClone(oldObj) {
+    let newObj = {};
+    for (let key in oldObj) {
+        if (typeof oldObj[key] === 'object'
+            && oldObj[key] !== null) {
+            newObj[key] = deepClone(oldObj[key]);
+            continue;
+        }
+        newObj[key] = oldObj[key];
+    }
+    return newObj;
+}
+
+getNewPassport.address.city = "Bobryisk";
+
+console.log(passportWithAddress);
+console.log(getNewPassport);
