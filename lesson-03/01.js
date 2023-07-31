c0b586de807a9cb002d4443243dc0a01447fce2a
kolyb <kolybenko@mail.ru>
2023-07-31 21:11:10 +0300

    Use filter and join

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,9 @@
 // code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString(colors) {
+    return colors.filter(item => item === "черный"
+    || item === "красный" || item === "желтый").join('-');
+}
+
+console.log(createColorString(colors));
