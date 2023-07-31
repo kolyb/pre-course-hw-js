ecff96499104e80c2b7cc0cd44ac87a7283e6539
kolyb <kolybenko@mail.ru>
2023-07-31 21:42:40 +0300

    Update 02.js

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,7 +1,6 @@
 // code
 const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
-if (words.indexOf("апельсин")) {
+const indexOrange = words.indexOf("апельсин");
+if (indexOrange === 2) {
     console.log("Ура! нашел");
-} else {
-    console.log("Придется поискать в другом магазине…");
-}
+} else console.log("Придется поискать в другом магазине…");
